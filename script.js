3c366a32aaa519f0f2aed97ef14a55f78a7ee5b0
yliuEmma <liu763@wisc.edu>
2022-03-08 11:52:44 -0500

    updated some code

---
+++ script.js
@@ -0,0 +1,83 @@
+const wordEl = document.getElementById('word');
+const wrongLettersEl = document.getElementById('wrong-letters');
+const playAgainBtn = document.getElementById('play-button');
+const popup = document.getElementById('popup-container');
+const notification = document.getElementById('notification-container');
+const finalMessage = document.getElementById('final-message');
+const finalMessageRevealWord = document.getElementById('final-message-reveal-word');
+
+const figureParts = document.querySelectorAll('.figure-part');
+
+const words = ['application', 'programming', 'interface', 'wizard'];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+const correctLetters = [];
+const wrongLetters = [];
+
+//show hidden word
+function displayWord(){
+    wordEl.innerHTML = `
+    ${selectedWord
+			.split('')
+			.map(
+				letter => `
+          <span class="letter">
+            ${correctLetters.includes(letter) ? letter : ''}
+          </span>
+        `
+			)
+			.join('')}
+  `;
+
+  const innerWord = wordEl.innerText.replace(/\n/g, '');
+
+  if(innerWord == selectedWord){
+      finalMessage.innerText='congrats you won';
+      popup.style.display = 'flex';
+  }
+}
+
+// update the wrong letters
+function updateWrongLettersEl(){
+    
+}
+
+//show notification
+function showNotification(){
+    notification.classList.add('show');
+
+    setTimeout(()=>{
+    
+        notification.classList.remove('show');
+
+    },2000);
+}
+
+//keydown letter press
+window.addEventListener('keydown', e => {
+	if (playable) {
+		if (e.keyCode >= 65 && e.keyCode <= 90) {
+			const letter = e.key.toLowerCase();
+
+			if (selectedWord.includes(letter)) {
+				if (!correctLetters.includes(letter)) {
+					correctLetters.push(letter);
+
+					displayWord();
+				} else {
+					showNotification();
+				}
+			} else {
+				if (!wrongLetters.includes(letter)) {
+					wrongLetters.push(letter);
+
+					updateWrongLettersEl();
+				} else {
+					showNotification();
+				}
+			}
+		}
+	}
+});
+displayWord();
